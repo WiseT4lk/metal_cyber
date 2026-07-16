f8daa994342b584638db7dc967bfc4e8552e8196
Thiago <wiset4lk@gmail.com>
2026-07-16 16:31:56 -0300

    cabecalho

---
+++ scripts/menu.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById("menu");
+const botao = document.getElementById("menuToggle");
+
+botao.addEventListener("click", () => {
+
+    menu.classList.toggle("active");
+
+    if(menu.classList.contains("active")){
+
+        botao.innerHTML = "✕";
+
+    }else{
+
+        botao.innerHTML = "☰";
+
+    }
+
+});
